eae8a997a9678a3d17ea6db53936dd5b8d188c05
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-16 23:39:15 +0100

    Rédaction de la javadoc
    Javadoc de la classe FavorableCase a été rédigée avant son implémentation.

--- src/main/kotlin/fr/songbird/sdk/probabuilder/FavorableCase.kt
+++ src/main/kotlin/fr/songbird/sdk/probabuilder/FavorableCase.kt
@@ -4,6 +4,31 @@ package fr.songbird.sdk.probabuilder
  * Représente un objet de manière générique.
  * Tous les objets passés en paramètre à cette classe doivent obligatoirement implémenter le service equals()
  * pour fonctionner correctement.
+ * Si vous utilisez des classes personnalisées lorsque vous faites appel aux services du gestionnaire
+ * de probabilités, prévoyez d'adapter l'implémentation de la méthode `equals()` en conséquence.
+ * Exemple:
+ *
+ * ```
+ * class Foo
+ * {
+ *     @Override
+ *     public boolean equals(Foo foo)
+ *     {
+ *         if(foo.getId() == this.getId())
+ *             return true;
+ *         return false;
+ *     }
+ * }
+ * ```
+ *
+ * Prenez bien garde à ceci car le système ne vous le rappelera pas.
+ *
+ * @author songbird
+ * @since 12 janv., 2017
+ * @param T Le type de l'item. **Note**: L'item doit être du même type que celui passé à une instance
+ * de la classe ProbabilityManager.
+ *
+ * @see ProbabilityManager
  */
 class FavorableCase<T> {
 }
