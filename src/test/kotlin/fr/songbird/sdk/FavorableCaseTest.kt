f422d955fa4953ec7a822fc76b9a2f6ee00c44b6
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-18 02:44:07 +0100

    FavorableCaseTest maj
    Rédaction des premiers tests unitaires

--- src/test/kotlin/fr/songbird/sdk/FavorableCaseTest.kt
+++ src/test/kotlin/fr/songbird/sdk/FavorableCaseTest.kt
@@ -1,7 +1,22 @@
 package fr.songbird.sdk.probabuilder
 
+import org.junit.Before
+import org.junit.Test
+
 /**
  * Created by anthony on 13/01/17.
  */
 class FavorableCaseTest {
+
+    @Test(expected = Exception::class)
+    fun negative_favorable_case_percentage()
+    {
+        FavorableCase("my awesome item", "item content", -1)
+    }
+
+    @Test(expected = Exception::class)
+    fun null_favorable_case_percentage()
+    {
+        FavorableCase("my awesome item", "item content", 0)
+    }
 }
