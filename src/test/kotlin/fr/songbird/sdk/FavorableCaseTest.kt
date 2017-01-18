f8d347ae83315ea38a94a30064374f4912f26943
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-18 21:05:30 +0100

    Ajout de nouveaux tests
    Ces derniers tests consistent à vérifier si la méthode renvoie bien
    le résultat entier souhaité et, si ce n'est pas le cas, lève une exception.

--- src/test/kotlin/fr/songbird/sdk/FavorableCaseTest.kt
+++ src/test/kotlin/fr/songbird/sdk/FavorableCaseTest.kt
@@ -1,6 +1,6 @@
-package fr.songbird.sdk.probabuilder
+package fr.songbird.sdk
 
-import org.junit.Before
+import fr.songbird.sdk.probabuilder.FavorableCase
 import org.junit.Test
 
 /**
@@ -18,5 +18,62 @@ class FavorableCaseTest {
     fun null_favorable_case_percentage()
     {
         FavorableCase("my awesome item", "item content", 0)
+
+    }
+
+    @Test
+    fun get_favorable_case_to_int_equals_10()
+    {
+        //potential_case vaut 100
+        //partons sur une échelle simple
+        val f : FavorableCase<String> = FavorableCase("my awesome item", "item content", 10)
+        val favorable_case_to_int : Int = f.get_favorable_case_to_int(100)
+        val favorable_case_to_int_result : Int = 10
+        assert(favorable_case_to_int == favorable_case_to_int_result, {"Le nombre de cas favorables n'est pas égal à $favorable_case_to_int_result. == $favorable_case_to_int"})
+
+    }
+    @Test
+    fun get_favorable_case_to_int_equals_50()
+    {
+        //potential_case vaut 100
+        //partons sur une échelle simple
+
+        val f : FavorableCase<String> = FavorableCase("my awesome item", "item content", 50)
+        val favorable_case_to_int : Int = f.get_favorable_case_to_int(100)
+        val favorable_case_to_int_result : Int = 50
+        assert(favorable_case_to_int == favorable_case_to_int_result, {"Le nombre de cas favorables n'est pas égal à $favorable_case_to_int_result. == $favorable_case_to_int"})
+    }
+    @Test
+    fun get_favorable_case_to_int_equals_75()
+    {
+        //potential_case vaut 100
+        //partons sur une échelle simple
+        val f : FavorableCase<String> = FavorableCase("my awesome item", "item content", 75)
+        val favorable_case_to_int : Int = f.get_favorable_case_to_int(100)
+        val favorable_case_to_int_result : Int = 75
+        assert(favorable_case_to_int == favorable_case_to_int_result, {"Le nombre de cas favorables n'est pas égal à $favorable_case_to_int_result. == $favorable_case_to_int"})
+    }
+    @Test
+    fun get_favorable_case_to_int_equals_100()
+    {
+        //potential_case vaut 200
+        //partons sur une échelle simple
+        val f : FavorableCase<String> = FavorableCase("my awesome item", "item content", 50)
+        val favorable_case_to_int : Int = f.get_favorable_case_to_int(200)
+        val favorable_case_to_int_result : Int = 100
+        assert(favorable_case_to_int == favorable_case_to_int_result, {"Le nombre de cas favorables n'est pas égal à $favorable_case_to_int_result. == $favorable_case_to_int"})
+    }
+    @Test(expected = Exception::class)
+    fun get_favorable_case_to_int_equals_potential_case()
+    {
+        //potential_case vaut 100
+        //partons sur une échelle simple
+        //on attend ici une erreur
+        //Le nombre de cas favorables pour un item ne doit pas être égal
+        //au nombre de cas potentiels, puisque le tirage au sort
+        //aura une issue certaine.
+
+        val f : FavorableCase<String> = FavorableCase("my awesome item", "item content", 100)
+        val favorable_case_to_int : Int = f.get_favorable_case_to_int(100)
     }
 }
