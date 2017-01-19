2e2922c7ae663a7550bc3616c95124028a2d3296
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-19 04:18:35 +0100

    mise à jour du copyright

--- src/main/kotlin/fr/songbird/sdk/probabuilder/FavorableCase.kt
+++ src/main/kotlin/fr/songbird/sdk/probabuilder/FavorableCase.kt
@@ -1,3 +1,22 @@
+/*
+ *    SurvivalDevKit, descendante de la bibliothèque utilitaire TheBareMinimum, mais en moins crade. :)
+ *     Copyright (C) 2016  Defranceschi Anthony
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     If you need more information, feel free to contact me at chaacygg[at]gmail[dot]com.
+ */
 package fr.songbird.sdk.probabuilder
 
 import java.util.logging.Level
