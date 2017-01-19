c049631574458838111788b5d0a7bf5a78dec83e
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-19 20:11:00 +0100

    update copyright

--- src/main/kotlin/fr/songbird/sdk/probabuilder/ProbabilityManager.kt
+++ src/main/kotlin/fr/songbird/sdk/probabuilder/ProbabilityManager.kt
@@ -1,6 +1,6 @@
 /*
  *    SurvivalDevKit, descendante de la bibliothèque utilitaire TheBareMinimum, mais en moins crade. :)
- *     Copyright (C) 2016  Defranceschi Anthony
+ *     Copyright (C) 2017  Defranceschi Anthony
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
