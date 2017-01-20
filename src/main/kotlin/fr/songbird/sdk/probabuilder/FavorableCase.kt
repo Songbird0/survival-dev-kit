455e1e32b938ab7e3846b969c1c15318a161dc7e
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-20 18:32:36 +0100

    Mise à jour de la javadoc
    Ce paragraphe était obsolète.
    L'utilisateur n'est plus obligé d'implémenter quoi que ce soit,
    la classe FavorableCase se charge d'implémenter equals pour vérifier selon ses propres conditions.
    
    Note: Obliger l'utilisateur à implémenter sa propre méthode était, à mon goût, une assez mauvaise idée.

--- src/main/kotlin/fr/songbird/sdk/probabuilder/FavorableCase.kt
+++ src/main/kotlin/fr/songbird/sdk/probabuilder/FavorableCase.kt
@@ -25,26 +25,6 @@ import java.util.logging.Logger
 
 /**
  * Représente un objet de manière générique.
- * Tous les objets passés en paramètre à cette classe doivent obligatoirement implémenter le service equals()
- * pour fonctionner correctement.
- * Si vous utilisez des classes personnalisées lorsque vous faites appel aux services du gestionnaire
- * de probabilités, prévoyez d'adapter l'implémentation de la méthode `equals()` en conséquence.
- * Exemple:
- *
- * ```
- * class Foo
- * {
- *     @Override
- *     public boolean equals(Foo foo)
- *     {
- *         if(foo.getId() == this.getId())
- *             return true;
- *         return false;
- *     }
- * }
- * ```
- *
- * Prenez bien garde à ceci car le système ne vous le rappelera pas.
  *
  * @author songbird
  * @since 12 janv., 2017
