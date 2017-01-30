bfcd010762e287783025302ec6651d1f24d6b205
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-30 19:24:00 +0100

    Rédaction de la documentation dédiée aux tests unitaires de la classe StringParserTest.

--- src/test/kotlin/fr/songbird/sdk/StringParserTest.java
+++ src/test/kotlin/fr/songbird/sdk/StringParserTest.java
@@ -22,54 +22,94 @@ package fr.songbird.sdk;
 
 import fr.songbird.sdk.stringparser.FileType;
 import fr.songbird.sdk.stringparser.StringParser;
+import fr.songbird.sdk.stringparser.listener.StringParserListener;
 import org.junit.Test;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
- * Created by anthony on 24/01/17.
+ * Classe dédiée aux tests de la classe {@link StringParser}.
  */
 public class StringParserTest {
 
 
+    /**
+     * On attend une erreur car aucune instance de la
+     * classe {@link java.util.List List} ou de ses filles
+     *  n'a été passée en paramètre.
+     */
     @Test(expected = RuntimeException.class)
     public void null_list()
     {
         new StringParser(null);
     }
 
+    /**
+     * On attend une erreur car la liste passée en
+     * paramètre ne contient aucun élément.
+     */
     @Test(expected = RuntimeException.class)
     public void empty_list()
     {
         new StringParser(new ArrayList<>());
     }
 
+    /**
+     * On attend une erreur car aucune instance de
+     * l'enum {@link FileType} n'a été passée en paramètre.
+     */
     @Test(expected = RuntimeException.class)
     public void file_type_is_null()
     {
         new StringParser(Paths.get("my", "awesome", "path"), null);
     }
 
+    /**
+     * On attend une erreur car aucune instance de
+     * la classe {@link java.nio.file.Path Path} n'a été passée en paramètre.
+     */
     @Test(expected = RuntimeException.class)
     public void path_to_database_is_null()
     {
         new StringParser(null, FileType.JSON);
     }
 
+    /**
+     * Procédure de test simple. Aucune assertion n'est nécessaire
+     * et il ne devrait rien se passer d'anormal (pas d'exceptions).
+     * On lance donc le constructeur qui permet d'utiliser un fichier en tant
+     * que base de données.
+     * @see StringParser#StringParser(Path, FileType)
+     */
     @Test
     public void use_a_file_as_database()
     {
         new StringParser(Paths.get(System.getProperty("user.dir")), FileType.JSON);
     }
 
+    /**
+     * Procédure de test simple. Aucune assertion n'est nécessaire
+     * et il ne devrait rien se passer d'anormal (pas d'exceptions).
+     * On lance le constructeur qui permet d'utiliser une liste en tant
+     * que base de données.
+     * @see StringParser#StringParser(List)
+     */
     @Test
     public void use_a_list_as_database()
     {
         new StringParser(Arrays.asList("Foo", "Bar", "Baz", "Bang"));
     }
 
+    /**
+     * Procédure de test simple. Aucune assertion n'est nécessaire
+     * et il ne devrait rien se passer d'anormal (pas d'exceptions).
+     * On lance le constructeur qui permet d'utiliser une liste en tant
+     * que base de données, puis on utilise la version surchargée de la méthode {@link StringParser#submit_pattern(String)}.
+     */
     @Test
     public void submit_pattern_with_list()
     {
