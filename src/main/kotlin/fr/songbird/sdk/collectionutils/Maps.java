896fea51bae0b8d58ccb648b01f5f1f89eee53d2
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-31 23:00:02 +0100

    Déclaration et implémentation de la méthode asMap().

--- src/main/kotlin/fr/songbird/sdk/collectionutils/Maps.java
+++ src/main/kotlin/fr/songbird/sdk/collectionutils/Maps.java
@@ -20,6 +20,9 @@
 
 package fr.songbird.sdk.collectionutils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * La classe Maps est destinée à fournir des services visant à raccourir
  * le temps d'écriture des opérations à effectuer
@@ -28,4 +31,36 @@ package fr.songbird.sdk.collectionutils;
  * @since 31 janv., 2017.
  */
 public final class Maps {
+
+
+    public static <K, V> Map<K, V> asMap(Entry<K, V> first_entry, Entry<K, V>... more_entries)
+    {
+        if(first_entry == null)
+            throw new RuntimeException("La référence first_entry est nulle.");
+        if(more_entries == null)
+            throw new RuntimeException("La référence more_entries est nulle.");
+        if(more_entries.length == 0)
+            throw new RuntimeException("Le tableau more_entries est vide. Remplissez-le d'au moins un élément pour faire " +
+                    "disparaître cette erreur.");
+
+        // la première clé de la première
+        // entrée
+        final K first_key = first_entry.getKey();
+        // la première valeur de la première
+        // entrée
+        final V first_value = first_entry.getValue();
+
+        // la map qui va recevoir les entrées
+        // et sera renvoyée en fin de programme.
+        final Map<K, V> map = new HashMap<>();
+        map.put(first_key, first_value);
+
+        for(Entry entry: more_entries)
+        {
+            final K current_key = entry.getKey();
+            final V current_value = entry.getValue();
+            map.put(current_key, current_value);
+        }
+        return map;
+    }
 }
