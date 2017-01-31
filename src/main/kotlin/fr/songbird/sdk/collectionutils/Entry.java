604e8614ed933a4c865cf2a735270adc0c9cd4c3
Anthony Defranceschi <chaacygg@gmail.com>
2017-01-31 23:02:40 +0100

    Déclaraction de la classe Entry<K, V>.
    La classe Entry permet de récupérer le couple clé/valeur soumis par l'utilisateur pour ensuite le fournir aux services de la classe Maps qui peuvent en avoir besoin lors de leurs opérations.

---
+++ src/main/kotlin/fr/songbird/sdk/collectionutils/Entry.java
@@ -0,0 +1,27 @@
+/*
+ *    SurvivalDevKit, descendante de la bibliothèque utilitaire TheBareMinimum, mais en moins crade. :)
+ *     Copyright (C) 2017  Defranceschi Anthony
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     If you need more information, feel free to contact me at chaacygg[at]gmail[dot]com.
+ */
+
+package fr.songbird.sdk.collectionutils;
+
+/**
+ * Created by anthony on 31/01/17.
+ */
+public class Entry<K, V> {
+}
